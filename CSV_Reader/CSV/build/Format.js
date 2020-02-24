493d9eb0acbe8d80dc49bcad665bb4e086cba438
tuannguyen4991 <nguyenthientuan4991@gmail.com>
2020-02-24 09:20:39 +0700

    refactor

---
+++ CSV_Reader/CSV/build/Format.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=Format.js.map
--- CSV_Reader/CSV/src/index.ts
+++ CSV_Reader/CSV/src/index.ts
@@ -1,26 +1,38 @@
-import * as fs from 'fs';
+// import * as fs from 'fs';
+import {CsvFileReader} from './CsvFileReader';
+import {MatchReader} from './MatchReader';
+import {MatchResult} from './MatchResult';
 
-const matches = fs
-  .readFileSync(`${__dirname}/../football.csv`, {encoding: 'utf8'})
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+// const matches = fs
+//   .readFileSync(`${__dirname}/../football.csv`, {encoding: 'utf8'})
+//   .split('\n')
+//   .map((row: string): string[] => {
+//     return row.split(',');
+//   });
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-};
+const teamName = 'Man United';
 
-const teamName = 'Chelsea';
+const matchReader = new MatchReader(`${__dirname}/../football.csv`);
+matchReader.read();
 
 let manUnitedWins = 0;
-for(let match of matches) {
+for(let match of matchReader.data) {
   match[1] === teamName && match[5] === MatchResult.HomeWin ? manUnitedWins += 1 :
     (match[2] === teamName && match[5] === MatchResult.AwayWin ? manUnitedWins += 1 : '')
 }
-const filterMatch = matches.filter(x => (x[1] === teamName && x[5] === MatchResult.HomeWin) || (x[2] === teamName && x[5] === MatchResult.AwayWin));
-const firstElement = `${teamName} won ${manUnitedWins} games \n`;
-const arr = firstElement + filterMatch.join('\n');
-const writeMatches = fs.writeFileSync(`${__dirname}/../filterMatches.csv`, arr, {encoding: 'utf8'})
+
+console.log(`Man United won ${manUnitedWins} games`);
+
+// const filterMatch = matches.filter(x => (x[1] === teamName && x[5] === MatchResult.HomeWin) || (x[2] === teamName && x[5] === MatchResult.AwayWin));
+// const firstElement = `${teamName} won ${manUnitedWins} games \n`;
+// const arr = firstElement + filterMatch.join('\n');
+// const writeMatches = fs.writeFileSync(`${__dirname}/../${teamName}.csv`, arr, {encoding: 'utf8'})
+
+// var a = '10/08/2018'.split('/').map(val => {
+//   // console.log(val);
+//   return parseInt(val);
+// });
+
+// console.log(a);
+// var date = new Date(Date.UTC(a[2], a[1] - 1, a[0]));
+// console.log(date);
